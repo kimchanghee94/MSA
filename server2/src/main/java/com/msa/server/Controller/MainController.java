797538934a07debb83d2a66c2b5689f390596b7a
kimchanghee <kimchanghee94@naver.com>
2025-08-25 14:05:56 +0900

    #main 1. MSA 서버 2개 분리

--- server2/src/main/java/com/msa/server/Controller/MainController.java
+++ server2/src/main/java/com/msa/server/Controller/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @GetMapping("/ms1/first")
+    @GetMapping("/ms2/second")
     public String mainP(){
         return "main";
     }
